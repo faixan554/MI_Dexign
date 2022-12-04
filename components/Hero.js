3c0581b8afae60c3d23487cf2c4feefbc561b81f
faixan554 <faixan.malik554@gmail.com>
2022-12-04 15:50:19 +0500

    update

--- components/Hero.js
+++ components/Hero.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Hero() {
   return (
     <section className='h-96 bg-black text-white'>
-      <div className='p-20 pt-32 md:p-32 md:pt-44 '>
+      <div className='p-10 pt-52 md:p-32 md:pt-44 '>
         <h1 className='text-base font-sans tracking-tight pb-4'>UI | UX DEXIGN</h1>
-        <h1 className='text-5xl md:text-6xl font-sans font-semibold md:tracking-widest'>MI DESIGN</h1>
+        <h1 className='text-5xl md:text-6xl font-sans font-semibold md:tracking-widest'>MI DEXIGN</h1>
       </div>
     </section>
     
--- pages/about.js
+++ pages/about.js
@@ -4,9 +4,9 @@ function about() {
   return (
     <section className="text-white bg-black body-font pb-32">
       <h1 className='text-4xl md:text-6xl font-sans font-semibold md:tracking-wider pt-16 pl-16'>ABOUT ME</h1>
-      <h1 className="title-font sm:text-4xl text-2xl font-medium text-white pt-4 pl-16">A bit more about me</h1>
+      <h1 className="title-font sm:text-3xl text-2xl font-medium text-white pt-4 pl-16">A bit more about me</h1>
         <div className="mx-auto flex px-5 flex-col-reverse md:flex-row">
-          <div className="pt-10 md:pt-20 lg:flex-grow md:w-2/3 md:pr-16 flex flex-col md:items-start md:text-left md:pl-16 mb-16 md:mb-0 items-center text-center">
+          <div className="pt-10 md:pt-20 lg:flex-grow md:w-2/3 md:pr-16 flex flex-col md:items-start md:text-left md:pl-14 mb-16 md:mb-0 items-center text-center">
             <p className="mb-2 leading-relaxed">First of all, I wanted to say thanks for finding time to go through my page! I appreciate you considering me.</p>
             <p className="leading-relaxed">My name is Muhammad Irfan, and i'm a fresh UI/UX Designer. I love helping comapnies of all sized impact the world positively and bring their vision to life through great UI/UX. </p>
           </div>
