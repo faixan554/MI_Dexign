5afe3c9c73244bedf17bca91cfa8bd3a0eac77a5
faixan554 <faixan.malik554@gmail.com>
2022-12-05 18:13:10 +0500

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -4,30 +4,36 @@ import React from 'react'
 function Contact() {
   return (
     <section className="text-white bg-black body-font p-10">
-        <div className="container px-5 mx-auto">
+        <div className="px-5 mx-auto">
             <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-5xl font-semibold title-font mb-4 text-white">Contact Us</h1>
+            <h1 className="sm:text-3xl text-4xl font-semibold title-font mb-4 text-white">Contact Us</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">How may i assit you!</p>
             </div>
             <div className="lg:w-1/2 md:w-2/3 mx-auto">
             <div className="flex flex-wrap -m-2">
                 <div className="p-2 w-1/2">
                 <div className="">
-                    <label for="name" className="leading-7 text-sm text-white">Name</label>
-                    <input type="text" id="name" name="name" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    <label for="firstname" className="leading-7 text-sm text-white">First Name</label>
+                    <input type="text" id="firstname" name="firstname" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                 </div>
                 </div>
                 <div className="p-2 w-1/2">
-                <div className="">
-                    <label for="email" className="leading-7 text-sm text-white">Email</label>
-                    <input type="email" id="email" name="email" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-                </div>
+                    <div className="">
+                        <label for="lastname" className="leading-7 text-sm text-white">Last Name</label>
+                        <input type="text" id="lastname" name="lastname" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
                 </div>
                 <div className="p-2 w-full">
-                <div className="">
-                    <label for="message" className="leading-7 text-sm text-white">Message</label>
-                    <textarea id="message" name="message" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                    <div className="">
+                        <label for="email" className="leading-7 text-sm text-white">Email</label>
+                        <input id="email" name="email" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
                 </div>
+                <div className="p-2 w-full">
+                    <div className="">
+                        <label for="message" className="leading-7 text-sm text-white">Message</label>
+                        <textarea id="message" name="message" className="w-full bg-gray-400 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-slate-100 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                    </div>
                 </div>
                 <div className="p-2 w-full">
                 <button className="flex font-sans font-semibold mx-auto text-white border-2 border-white py-2 px-8 focus:outline-none hover:bg-white hover:text-black rounded-2xl text-lg">Submit</button>
